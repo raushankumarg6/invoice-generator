280d8679e52ff1fc1d154c90221db280d0f988d0
Raushan Kumar Gupta <66469791+raushankumarg6@users.noreply.github.com>
2022-12-16 20:23:26 +0530

    Update InvoiceItem.jsx

--- src/components/InvoiceItem.jsx
+++ src/components/InvoiceItem.jsx
@@ -33,20 +33,8 @@ const InvoiceItem = ({ id, name, qty, price, onDeleteItem, onEdtiItem }) => {
         />
       </td>
       <td className="relative min-w-[100px] md:min-w-[150px]">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute left-2 top-1/2 h-6 w-6 -translate-y-1/2 text-gray-400 sm:left-4"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
+        
+      
         <InvoiceField
           onEditItem={(event) => onEdtiItem(event)}
           cellData={{
